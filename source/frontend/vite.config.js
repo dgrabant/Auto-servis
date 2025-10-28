ec2992c90aab8dedd4e0f898d0ad3eb84570efaf
qright <antonio.santak@fer.unizg.hr>
2025-10-28 14:31:37 +0100

    Ažurirane konfiguracijske datoteke frontend-a
    Generiranje statičkih datoteka.

--- source/frontend/vite.config.js
+++ source/frontend/vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview:{
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
-    allowedHosts: ['autoservis-uwi7.onrender.com']
+  base: './',       // relative paths for static deployment
+  server: {
+    host: '0.0.0.0',  // for dev server if testing on Render's preview
+    port: 5173
+  },
+  build: {
+    outDir: 'dist',   // default output directory
+    emptyOutDir: true
   }
 })
