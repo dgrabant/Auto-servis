fee09f7dd13fec991e6c70e964d8f5086c9d8d59
Mihael Car <mihael.car@fer.unizg.hr>
2025-10-30 19:43:47 +0100

    Dorada poboljšanja tranzicije

--- source/frontend/src/js/Main/load.js
+++ source/frontend/src/js/Main/load.js
@@ -279,7 +279,7 @@ function onDocumentClick(event) {
       transitionCamera(activeCamera, cameraList[cameraPosition], 2000);
     }
     if (firstHitName.startsWith("djelovi") || firstHitName == "login") {
-      transitionCamera(activeCamera, cameraList[cameraPosition], 1000);
+      transitionCamera(activeCamera, cameraList[cameraPosition], 1100);
     }
     // *** SPREMANJE ID-A TIMERA ZA PRIJELAZ ***
     transitionTimeout = setTimeout(() => { 
