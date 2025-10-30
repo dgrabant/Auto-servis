b362ff70016134dcac6a1ccbd7224363a32cd725
Mihael Car <mihael.car@fer.unizg.hr>
2025-10-30 19:41:06 +0100

    Poboljšanje tranzicije na laptop

--- source/frontend/src/js/Main/load.js
+++ source/frontend/src/js/Main/load.js
@@ -279,7 +279,7 @@ function onDocumentClick(event) {
       transitionCamera(activeCamera, cameraList[cameraPosition], 2000);
     }
     if (firstHitName.startsWith("djelovi") || firstHitName == "login") {
-      transitionCamera(activeCamera, cameraList[cameraPosition], 1500);
+      transitionCamera(activeCamera, cameraList[cameraPosition], 1000);
     }
     // *** SPREMANJE ID-A TIMERA ZA PRIJELAZ ***
     transitionTimeout = setTimeout(() => { 
@@ -295,7 +295,7 @@ function onDocumentClick(event) {
             document.getElementById("navDjelovi").hidden = true;
         }
       }
-    }, 1000);
+    }, 1250);
   
   }
 }
