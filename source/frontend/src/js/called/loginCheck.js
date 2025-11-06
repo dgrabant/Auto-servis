1799c129db1a6f3d9f5ed021944def60606039c7
Mihael Car <mihael.car@fer.unizg.hr>
2025-11-06 21:11:07 +0100

    Dorada 5

--- source/frontend/src/js/called/loginCheck.js
+++ source/frontend/src/js/called/loginCheck.js
@@ -5,13 +5,13 @@ export function checkIfLogedIn(){
         // TOKEN JE PRONAĐEN (Korisnik je prijavljen)
         console.log('Korisnik je prijavljen. Token: ', authToken);
         // Ovdje učitajte sadržaj za prijavljenog korisnika (npr. 'Welcome Dashboard')
-        document.getElementById('app-container').innerHTML = '<h2>Dobrodošli! Vi ste prijavljeni.</h2>';
+        //document.getElementById('app-container').innerHTML = '<h2>Dobrodošli! Vi ste prijavljeni.</h2>';
         return true;
     } else {
         // TOKEN NIJE PRONAĐEN (Korisnik nije prijavljen)
         console.log('Korisnik nije prijavljen.');
         // Ovdje učitajte sadržaj za neprijavljenog korisnika (npr. login/register forme)
-        document.getElementById('app-container').innerHTML = '<h2>Molimo prijavite se.</h2>';
+        //document.getElementById('app-container').innerHTML = '<h2>Molimo prijavite se.</h2>';
         return false;
         }
     }
