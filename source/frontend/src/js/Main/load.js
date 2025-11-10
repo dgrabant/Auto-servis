62047fa52112ef910f69b7204e855636e56ce5a2
Mihael Car <mihael.car@fer.unizg.hr>
2025-11-10 12:50:29 +0100

    Popravak grešaka

--- source/frontend/src/js/Main/load.js
+++ source/frontend/src/js/Main/load.js
@@ -77,8 +77,9 @@ function provjera() {
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
+                resolve();
             }
-            resolve();
+              return response.json();
             })
         .then(data => {
             console.log("Response data:", data);
