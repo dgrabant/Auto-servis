e09fbb625eebbac337f272d0713b026aa9b382e1
antonio-santak <antonio.santak@fer.unizg.hr>
2026-01-10 15:29:06 +0100

    Ažurirano pristupanje API-u

--- source/backend/src/main/java/hr/fer/progi/autoservis/dto/TerminUpdateDto.java
+++ source/backend/src/main/java/hr/fer/progi/autoservis/dto/TerminUpdateDto.java
@@ -1,16 +1,13 @@
 package hr.fer.progi.autoservis.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TerminUpdateDto {
-    @NotNull
     private Integer idKorisnik;
 
-    @NotNull
     private String datumVrijeme;
 
     private String odgoda;
